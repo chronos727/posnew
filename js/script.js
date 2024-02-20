91fe344e7154689770191cb01f00c21ffbbb6a4a
Golgrax <70707230+Golgrax@users.noreply.github.com>
2024-02-20 22:33:01 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -172,17 +172,22 @@ function show_invoice(){
                 <h6 class="mt-0">${val[1]}</h6>
                 <p>PHP <span>${val[2]}</span></p>
                 </div>
-                <p><span>Quantity </span></p>
+                <p>Quantity </p>
                 <input class="quantity mt-3" id="quantity" type="number" value="${Number(val[3])}">
                 <button class="btn delete mt-2"><img src="https://img.icons8.com/wired/35/000000/trash.png"></button>
             </div>
         `
         const el_total = `
             <div class="media mb-2">
-                <img src="assets/${val[0]}" class="align-self-center mr-3" width="95">
-                <div class="media-body">
+
+
+
+
+                
+                 <div class="media-body">
                     <h6 class="mt-0">${val[1]}</h6>
                     <p>PHP <span>${val[2]}</span></p>
+                    <p>Quantity </p>
                     <span>${val[3]}</span>
                 </div>
             </div>
